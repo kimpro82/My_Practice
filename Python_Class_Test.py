0bcb246156d880092a166fe18bd9afa4d283b8be
Kangrok Kim <kimpro1010@gmail.com>
2018-02-07 18:04:40 +0900

    Create Python_Class_Test.py

---
+++ Python_Class_Test.py
@@ -0,0 +1,14 @@
+class MyFirstClass :
+    
+    def Family(self, name, role):
+        print(name, "is a(an)", role, "in my family")
+
+Do = MyFirstClass()
+
+Do.Family("Kim", "Husband")
+Do.Family("Shin", "Wife")
+Do.Family("Kim", "Future Baby")
+
+# I found that a simple class in Python 
+# doesn't need stuffs like __main__, __init__ and so on.
+# What the __hell__?
