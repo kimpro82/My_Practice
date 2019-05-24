33d3161d47a09acfe01aa5f981750e5d5a0e9cca
Kangrok Kim <kimpro1010@gmail.com>
2019-05-24 12:14:15 +0900

    Add files via upload

---
+++ password.py
@@ -0,0 +1,25 @@
+import time # for using time.sleep()
+
+chance = 0
+pw_original = "mymy" # password. a word that call a pass. you nahm sayin?
+
+while chance < 5 :
+    pw_input = input("Input your password : ")
+
+    # right
+    if pw_original == pw_input :
+        print("You entered the correct password")
+        break
+    
+    # wrong
+    else:
+        chance += 1
+        print("You entered the wrong passwords", chance, "times.")
+        if chance == 5 :
+            print("You bady guys will be delayed as a penalty.")
+            time.sleep(3)
+        else :
+            continue
+
+# Of course, saving the original password in this file is somewhat stupid.
+# But, yes I am.
