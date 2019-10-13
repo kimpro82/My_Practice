00170c1375de1bdd9e4a3cd4b9b96b2951cefae3
Kangrok Kim <kimpro1010@gmail.com>
2019-10-14 01:14:40 +0900

    TCP/UDP Server-Client Programming Basic Practice (#26)
    * Create TCP_Server.py
    * Create TCP_Client.py
    * Create UDP_Server.py
    * Create UDP_Client.py
    
    * Update README.md

---
+++ Python_Network/TCP_Client.py
@@ -0,0 +1,24 @@
+# Each server/client side codes should be run different kernel from each other.
+
+# 2. TCP Client
+
+from socket import * # import socket module
+
+# declare variables of ip and port address
+server_ip, server_port = "127.0.0.1", 12345
+
+client_sock = socket(AF_INET, SOCK_STREAM) # generate a socket instance
+
+# connect to the server with binding ip and port address
+client_sock.connect((server_ip, server_port))
+print("연결이 확인되었습니다.")
+
+# send data to the server
+client_sock.send("저는 클라이언트입니다.".encode('utf-8'))
+print("메세지를 전송했습니다.")
+
+# recieve data from the server
+data = client_sock.recv(1024)
+print("받는 데이터 :", data.decode('utf-8'))
+
+client_sock.close() # close the client's socket
---
+++ Python_Network/UDP_Server.py
@@ -0,0 +1,22 @@
+# Each server/client side codes should be run different kernel from each other.
+
+# 3. UDP Server
+
+from socket import * # import socket module
+
+# declare variables of ip and port address
+server_ip, server_port = "127.0.0.1", 12345
+
+server_sock = socket(AF_INET, SOCK_DGRAM) # generate a socket instance
+server_sock.bind((server_ip, server_port)) # bind ip and port address
+
+server_sock.settimeout(10) # define timeout limit : "10" seconds
+
+# recieve data from the client within '1024' byte
+data, client_addr = server_sock.recvfrom(1024)
+
+# print the client's IP address and recieved data
+print("보낸 IP : ", client_addr)
+print("받은 데이터 : ", data.decode())
+
+server_sock.close() # close the server
