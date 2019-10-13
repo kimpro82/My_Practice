8dc72e490280ea5fe612bb900c315529b5939826
Kangrok Kim <kimpro1010@gmail.com>
2019-10-14 00:55:55 +0900

    Create UDP_Client.py

---
+++ Python_Network/UDP_Client.py
@@ -0,0 +1,16 @@
+# Each server/client side codes should be run different kernel from each other.
+
+# 4. UDP Client
+
+from socket import * # import socket module
+
+# declare variables of ip and port address
+server_ip, server_port = "127.0.0.1", 12345
+
+# generate a socket instance
+client_sock = socket(AF_INET, SOCK_DGRAM) 
+
+# send data to the server
+client_sock.sendto("Hi! Server.".encode(),(server_ip, server_port))
+
+client_sock.close() # close the client's socket
