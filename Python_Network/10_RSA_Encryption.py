1f9a863786b0d8197c738ebfd8c9a67a70f8e396
Kangrok Kim <kimpro1010@gmail.com>
2019-12-03 02:53:24 +0900

    Update 10_RSA_Encryption.py

--- Python_Network/10_RSA_Encryption.py
+++ Python_Network/10_RSA_Encryption.py
@@ -4,7 +4,7 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-# function of RSA enctyption
+# function of RSA encryption
 def rsa_enc(msg) :
     private_key = RSA.generate(1024)
         # 1024 : must be a multiple of 256 and >=1024
