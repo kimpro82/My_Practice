bc8c4829725bab23e75cd86e3343e2e10af4bd3f
Kangrok Kim <kimpro1010@gmail.com>
2019-12-22 02:59:05 +0900

    Update Fibonacci_Series.py (#45)
    * Update Fibonacci_Series.py
    * Update README.md

--- Python/Fibonacci_Series.py
+++ Python/Fibonacci_Series.py
@@ -1,10 +1,10 @@
 # Generating Fibonacci Series by Python
 
 a = [1, 1]
-n = 1
+n = 2
 
-while n<10 :
-    a.append(a[n-1] + a[n])
+while n<10 : # length = 10
+    a.append(a[n-2] + a[n-1])
     n += 1
 
 print(a)
