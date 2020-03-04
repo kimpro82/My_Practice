b9ca3a1d3a391e8719142f08ff560261041a4f40
Kangrok Kim <kimpro1010@gmail.com>
2020-03-05 01:12:55 +0900

    Update ColorfulShow.js

--- Web/ColorfulShow.js
+++ Web/ColorfulShow.js
@@ -1,16 +1,19 @@
 function changeColor() {
-	randColor = '#' + Math.floor(Math.random() * Math.pow(256, 3));
+	randNumDec = Math.floor(Math.random() * Math.pow(256, 3));
 		/*
-			Math.random() returns a number lower than 1.
-	 		Math.floor() returns the largest integer less than or equal to a given number.
-			256**3 is for the RGB color range between #000000 ~ #FFFFFF.
-		*/
-	document.write(randColor, "<br>");
-		/* document.write() returns real HTML codes. */
-	document.write(typeof randColor);
-	document.getElementById('chameleon').style.color.disabled
-	document.getElementById('chameleon').style.color = randColor;
+		 *	Math.random() returns a number lower than 1.
+	 	 *	Math.floor() returns the largest integer less than or equal to a given number.
+		 *	256**3 is for the RGB color range between #000000 ~ #FFFFFF.
+		 */
+	randNumHex = randNumDec.toString(16); /* Convert Decimal to Hexadecimal */
 
+	/* document.write(randNumHex, "<br>"); */
+		/* document.write() returns real HTML codes. */
+		/* document.write(typeof randColor);
+		 * string
+		 */
+
+	document.getElementById('chameleon').style.color = '#' + randNumHex;
 }
 
-changeColor();
+setInterval(changeColor, 1000);
