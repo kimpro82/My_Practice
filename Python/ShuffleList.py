da2a3d2ab05fc5eb55d60ec156890e33183c6b44
Kangrok Kim <kimpro1010@gmail.com>
2020-10-20 04:55:18 +0900

    Update ShuffleList.py

--- Python/ShuffleList.py
+++ Python/ShuffleList.py
@@ -1,21 +1,34 @@
 import random
 
+
+# Trial 1
+
 sufflelist = []
 
-# for i in range(0,20) :
-#   sufflelist.append(random.randint(1, 20))
+for i in range(0,20) :
+    sufflelist.append(random.randint(1, 20))
 
-# print(sufflelist)
+print(sufflelist) # There are overlapping values.
 
-# for i in range(0,20) :
-#     sufflelist.append(random.sample(range(1, 20), 1))
 
-# print(sufflelist)
+# Trial 2
 
+sufflelist = []
+
+for i in range(0,20) :
+    sufflelist.append(random.sample(range(1, 20), 1))
+
+print(sufflelist)
+
+# The values are in nested lists and overlapping ones still remains.
+
+
+# Trial 3
 
 list=[]
+
 while len(list) < 10 :
     r = random.randint(1,100)
     if r not in list: list.append(r)
 
-print(list)
+print(list) # There's no more overlapping values.
