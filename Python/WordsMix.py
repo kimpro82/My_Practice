5547f5e6efe819a65122e6010ddc2e2d863d64af
Kangrok Kim <kimpro1010@gmail.com>
2021-01-14 00:38:25 +0900

    Create WordsMix.py

---
+++ Python/WordsMix.py
@@ -0,0 +1,37 @@
+# 0. Check If Words.csv Exists
+
+import os
+
+path = "C:\\Users\\fya\\Documents\\신랑\\개발\\Python\\Words.csv"
+os.path.isfile(path)
+
+
+# 1. Read Words.csv simply 
+
+import csv
+
+with open(path,'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+
+    for c in reader:
+        for k, v in c.items():
+            print(v, end= ' ')
+        print("\n")
+
+
+# 1-1. Read Words.csv as dictionary type
+
+with open(path,'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+
+    for row in reader:
+        print(row)
+
+
+# 1-2. Get rid of '\ufeff' from the head of data
+
+with open(path,'r', encoding='utf-8-sig') as f:
+    reader = csv.DictReader(f)
+
+    for row in reader:
+        print(row)
