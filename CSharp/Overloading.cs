49badcbe3be0e87054b6b00fbb7a5fa003191f4d
Kangrok Kim <kimpro1010@gmail.com>
2021-02-02 06:14:38 +0900

    Update Overloading.cs

--- CSharp/Overloading.cs
+++ CSharp/Overloading.cs
@@ -6,18 +6,25 @@ class Wife
   public string var1, var2;
   public int num1;
 
+  public Wife() {
+    Console.WriteLine("우리 마누라는 …… 노코멘트.");
+  }
+
   public Wife(string var1) {
     this.var1 = var1;
+    Console.WriteLine("우리 마누라는 " + this.var1 + "입니다.");
   }
 
   public Wife(string var1, string var2)
   {
     this.var1 = var1;
     this.var2 = var2;
+    Console.WriteLine("우리 마누라는 " + this.var1 + "(이)며 " + this.var2 + "입니다.");
   }
 
   public Wife(int num1) {
     this.num1 = num1;
+    Console.WriteLine("우리 마누라는 " + this.num1 + "살 먹은 여우입니다.");
   }
 
 }
@@ -29,13 +36,10 @@ class MainClass
   static void Main (string[] args)
   {
 
-    Wife Wife1 = new Wife("바보");
-    Wife Wife2 = new Wife("멍청이", "말미잘");
-    Wife Wife3 = new Wife(100);
-
-    Console.WriteLine("우리 마누라는 " + Wife1.var1 + "입니다.");
-    Console.WriteLine("우리 마누라는 " + Wife2.var1 + "(이)며 " + Wife2.var2 + "입니다.");
-    Console.WriteLine("우리 마누라는 " + Wife3.num1 + "살 먹은 여우입니다.");
+    new Wife();
+    new Wife("바보");
+    new Wife("멍청이", "말미잘");
+    new Wife(100);
 
   }
 
