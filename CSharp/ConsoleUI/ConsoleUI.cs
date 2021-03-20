dc9960a5b1b2eda3b66df0c5854c8b28acb9d092
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-20 18:28:51 +0900

    set .gitignore to prevent bin\, obj\

--- CSharp/ConsoleUI/ConsoleUI.cs
+++ CSharp/ConsoleUI/ConsoleUI.cs
@@ -15,11 +15,40 @@ namespace ConsoleUI
         {
 
             Application.Init ();
+            var top = Application.Top;
 
-            var n = MessageBox.Query (50, 7,
-                "Question", "Do you like console apps?", "Yes", "No");
+            // Creates the top-level window to show
+            var win = new Window (new Rect (0, 1, top.Frame.Width, top.Frame.Height-1), "MyApp");
+            top.Add (win);
 
-            return n;
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar (new MenuBarItem [] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_New", "Creates new file", ()=> {}),
+                    new MenuItem ("_Close", "", () => {}),
+                    new MenuItem ("_Quit", "", () => { top.Running = false; })
+                }),
+                new MenuBarItem ("_Edit", new MenuItem [] {
+                    new MenuItem ("_Copy", "", null),
+                    new MenuItem ("C_ut", "", null),
+                    new MenuItem ("_Paste", "", null)
+                })
+            });
+            top.Add (menu);
+
+            // Add some controls
+            win.Add (
+                new Label (3, 2, "Login: "),
+                new TextField (14, 2, 40, ""),
+                new Label (3, 4, "Password: "),
+                new TextField (14, 4, 40, "") {  },
+                new CheckBox (3, 6, "Remember me"),
+                new RadioGroup (3, 8, new [] { "_Personal", "_Company" }),
+                new Button (3, 14, "Ok"),
+                new Button (10, 14, "Cancel"),
+                new Label (3, 18, "Press ESC and 9 to activate the menubar"));
+
+            Application.Run ();
 
         }
 
