784de95239922212713697ae183c9e31f18fcfcf
Kangrok Kim <kimpro@uebermensch.tech>
2021-03-24 08:55:16 +0900

    Update Turtle.py and README.md

--- Python/Turtle.py
+++ Python/Turtle.py
@@ -1,5 +1,5 @@
 import turtle
-
+import time
 
 turtle.setup(width = 300, height = 300)
 turtle.title("My turtle practice")
@@ -11,18 +11,38 @@ turtle.home()               # set the position (0, 0)
 turtle.position()
 
 turtle.penup()              # penup() = pu() = up() : move without drawing
-turtle.setpos(0, -100)
+turtle.setpos(0, 125)
 
 turtle.pendown()            # pendown() = pd() = down() : move with drawing
-turtle.circle(100)
+turtle.right(180)
+turtle.circle(125)          # 1st circle
+
+turtle.penup()
+turtle.setpos(0, 100)
+
+turtle.pendown()
+time.sleep(0.3)
+turtle.circle(100)          # 2st circle
+
+turtle.delay(20)
+
+time.sleep(0.5)
 turtle.circle(100, steps=3) # 1st triangle
+
+turtle.penup()
+turtle.setpos(0, -100)
+turtle.right(180)
+
+turtle.pendown()
+turtle.circle(100, steps=3) # 2nd triangle
 
 turtle.penup()
 turtle.setpos(0, 100)
 turtle.right(180)
 
+turtle.delay(30)
+
 turtle.pendown()
-turtle.circle(100, steps=3) # 2nd triangle
 turtle.circle(100, steps=6) # hexagon
 
 
