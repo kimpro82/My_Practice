217bf2d6fbfc4e0722a8006d3ae39c58f70248a7
Kangrok Kim <kimpro1010@gmail.com>
2021-05-06 08:44:25 +0900

    Python - If & While True (#115)
    * Create IfWhileTrue.py and Update README.md

---
+++ Python/IfWhileTrue.py
@@ -0,0 +1,37 @@
+# If
+
+if True :
+    print(True)
+
+if False :
+    print(False)
+
+if 'abc' :
+    print('abc')
+
+a = 1
+if a :
+    print(a)
+
+b = 0
+if b :
+    print(b)
+
+c = -1
+if c :
+    print(c)
+
+
+# While
+
+while True :
+    print(True)
+    break
+
+while False :
+    print(False)
+    break
+
+while '123' :
+    print('123')
+    break
