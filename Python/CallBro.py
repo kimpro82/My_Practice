ecf407c917f200f62af7e983447c3b13da43ac33
kimpro82 <kimpro@uebermensch.tech>
2021-06-17 00:38:28 +0900

    Create CllBro.py

---
+++ Python/CallBro.py
@@ -0,0 +1,11 @@
+class marriage() :
+
+    def wife(self) :
+        print("Yes")
+    
+    def husband(self) :
+        print("Will you marry me?")
+        marriage().wife()
+
+if __name__ == "__main__" :
+    marriage().husband()
