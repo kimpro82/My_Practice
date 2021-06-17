640463ca792b0bfcaf910247593bbdec1bae337d
kimpro82 <kimpro@uebermensch.tech>
2021-06-18 01:27:28 +0900

    Update CllBro.py

--- Python/CallBro.py
+++ Python/CallBro.py
@@ -1,11 +1,30 @@
-class marriage() :
+class Bros :
 
-    def wife(self) :
-        print("Yes")
-    
-    def husband(self) :
-        print("Will you marry me?")
-        marriage().wife()
+    def __init__(self) :
+        self.conversation = iter(["Hey bro", "Wassup"])
+        self.n = 0
+
+    def bros1(self) :
+        print(self.bros1.__name__ + " : " + next(self.conversation))
+        if (self.n < 1) :
+            self.n += 1
+            self.bros2()
+        else :
+            print()
+
+    def bros2(self) :
+        print(self.bros2.__name__ + " : " + next(self.conversation))
+        if (self.n < 1) :
+            self.n += 1
+            self.bros1()
+        else :
+            print()
+
 
 if __name__ == "__main__" :
-    marriage().husband()
+
+    Bros1 = Bros()
+    Bros1.bros1()
+
+    Bros2 = Bros()
+    Bros2.bros2()
